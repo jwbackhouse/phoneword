0250869e97690a8d426d7c1cf6c68bebe8514ae5
jwbackhouse <jwbackhouse@gmail.com>
2020-10-12 11:45:42 +0000

    Setup POST call to Express.

--- client/src/components/Converter.js
+++ client/src/components/Converter.js
@@ -3,18 +3,16 @@ import axios from 'axios';
 import InputForm from './InputForm.js';
 
 const Converter = props => {
-  const [input, setInput] = useState('');
-  const handleChange = e => {
-    const char = e.target.value;
-    const check = /[0-9]/;
-    if (!check.test(char)) return;
-    setInput(e.target.value);
+  const getResult = query => {
+    axios.post('/convert', { input: query })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
   };
 
   return (
     <div>
       <h1>Hello World</h1>
-      <InputForm />
+      <InputForm getResult={ getResult }/>
     </div>
   );
 };
--- client/src/components/InputForm.js
+++ client/src/components/InputForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const InputForm = props => {
+const InputForm = ({ getResult }) => {
   const [input, setInput] = useState('');
   const handleChange = e => {
     // Restrict input to numbers only
@@ -11,15 +11,25 @@ const InputForm = props => {
     setInput(e.target.value);
   };
 
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    getResult(input);
+    setInput('');
+  };
+
   return (
-    <form>
+    <form onSubmit={ handleSubmit } >
       <input
         className='input'
         name='numbers'
         type='text'
         value={ input }
-        onChange = { handleChange }
+        onChange={ handleChange }
       />
+      <button type='submit'>
+        Go
+      </button>
     </form>
   );
 };
