e223e45497da81b67db6e397f97d1e354e90e063
jwbackhouse <jwbackhouse@gmail.com>
2020-10-12 11:43:10 +0000

    Resolve port issues for Cloud9 / EC2 environment.

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = require('./src/routers/router.js');
 
 const app = express();
-// const port = process.env.PORT || 3000;
-const port = 3000;
+const port = process.env.PORT || 8080;
 
 // Handle CORS issues
 app.use(express.json());
