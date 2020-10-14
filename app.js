3a32dc5cd2e27794255225b32417afdae14b22d3
jwbackhouse <jwbackhouse@gmail.com>
2020-10-14 11:40:44 +0000

    Move server launch to new file to facilitate testing.

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = require('./src/routers/router.js');
 
 const app = express();
-const port = process.env.PORT || 8080;
 
 // Handle CORS issues
 app.use(express.json());
@@ -13,6 +12,4 @@ app.use((req, res, next) => {
 });
 app.use(router);
 
-app.listen(port, () => {
-  console.log('Server is running on port ' + port);
-});
+module.exports = app;
