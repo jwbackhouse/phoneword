697e9ff69fa500243a4d18c9d3eef5554e3c32b9
jwbackhouse <jwbackhouse@gmail.com>
2020-10-14 11:42:33 +0000

    Delete redundant code & update dependency path.

--- src/tests/server.test.js
+++ src/tests/server.test.js
@@ -1,7 +1,6 @@
 const router = require('../routers/router.js');
 const express = require('express');
-// const moxios = require('moxios');
-const app = require('../../index.js');
+const app = require('../../app.js');
 const supertest = require('supertest');
 
 const request = supertest(app);
@@ -19,52 +18,5 @@ describe('/GET endpoint', () => {
     expect(res.statusCode).toEqual(400);
     expect(res.body).toBeEmpty;
     done();
-  })
+  });
 });
-
-
-// const initRouter = () => {
-//   const app = express();
-//   app.use(router);
-//   return app;
-// };
-
-// describe('GET /convert/:query', () => {
-//   beforeEach(() => {
-//     moxios.install();
-//   });
-//   afterEach(() => {
-//     moxios.uninstall();
-//   });
-//   test('Should return response to query '23'', async () => {
-//     moxios.stubRequest(/18.133.60.101:8082\/convert\/23/, {
-//       status: 200,
-//       response: {
-//         data: ['ad', 'ae', 'af']
-//       }
-//     });
-//     const app = initRouter();
-//     await request(app).get('/convert/23');
-//     expect(moxios.requests.mostRecent().url).toBe('http://18.133.60.101:8082/convert/23');
-//   });
-
-//   // test('It should 200 and return a transformed version of GitHub response', async () => {
-//   //   moxios.stubRequest(/api.github.com\/users/, {
-//   //     status: 200,
-//   //     response: {
-//   //       blog: 'https://codewithhugo.com',
-//   //       location: 'London',
-//   //       bio: 'Developer, JavaScript',
-//   //       public_repos: 39,
-//   //     }
-//   //   });
-//   //   const app = initHugo();
-//   //   const res = await request(app).get('/hugo');
-//   //   expect(res.body).toEqual({
-//   //     blog: 'https://codewithhugo.com',
-//   //       location: 'London',
-//   //       bio: 'Developer, JavaScript',
-//   //       publicRepos: 39,
-//   //   });
-//   // });
-// });
