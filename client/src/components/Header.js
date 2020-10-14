aa1e2bcf22cd3370003f77078c70cd25588b289a
jwbackhouse <jwbackhouse@gmail.com>
2020-10-14 12:13:18 +0000

    Update title copy.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = props => (
   <header>
-    <h1 className='title'>Greetings from the 1990s</h1>
+    <h1 className='title'>Greetings from the 1990s!</h1>
     <p className='subtitle'>Find the letter combinations from your mobile keypad</p>
   </header>
 );
